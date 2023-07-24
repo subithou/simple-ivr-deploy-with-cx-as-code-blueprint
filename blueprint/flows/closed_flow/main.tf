ad84f1ff08c7738f83872d8f3e745d5f3767bad0
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-24 10:33:10 +0000

    up closed hours

--- blueprint/flows/closed_flow/main.tf
+++ blueprint/flows/closed_flow/main.tf
@@ -15,8 +15,8 @@ provider "genesyscloud" {
 
 
 resource "genesyscloud_flow" "mysimpleflow3" {
-  filepath = "./Closed Hours5.yaml"
-  file_content_hash = filesha256("./Closed Hours5.yaml") 
+  filepath = "./Closed Hours6.yaml"
+  file_content_hash = filesha256("./Closed Hours6.yaml") 
 }
 
 
