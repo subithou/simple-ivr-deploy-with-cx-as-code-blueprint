ec156016d572e567c5e78cae9b0aec7fde169fd2
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-25 06:14:16 +0000

    display all users in finance ivr

--- blueprint/flows/finance_ivr/script/platform_tests.py
+++ blueprint/flows/finance_ivr/script/platform_tests.py
@@ -4,8 +4,27 @@ import os
 import time
 import PureCloudPlatformClientV2
 
+api_client = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token('682b4a94-5e61-4219-b399-e52764c8da30','9c7Op9xuqC2NXlgUfXywP5YyClYzEbhu_FChShRQOtY')
 
-print("Hello")
+api_instance = PureCloudPlatformClientV2.UsersApi(api_client);
+
+page_size = 25 # int | Page size (optional) (default to 25)
+page_number = 1 # int | Page number (optional) (default to 1)
+# id = ['id_example'] # list[str] | A list of user IDs to fetch by bulk (optional)
+# jabber_id = ['jabber_id_example'] # list[str] | A list of jabberIds to fetch by bulk (cannot be used with the \"id\" parameter) (optional)
+sort_order = ''ASC'' # str | Ascending or descending sort order (optional) (default to 'ASC')
+# expand = ['expand_example'] # list[str] | Which fields, if any, to expand (optional)
+# integration_presence_source = 'integration_presence_source_example' # str | Gets an integration presence for users instead of their defaults. This parameter will only be used when presence is provided as an \"expand\". When using this parameter the maximum number of users that can be returned is 100. (optional)
+state = 'active' # str | Only list users of this state (optional) (default to 'active')
+
+try:
+    # Get the list of available users.
+    api_response = api_instance.get_users(page_size=page_size, page_number=page_number, sort_order=sort_order, state=state)
+    print(api_response)
+except ApiException as e:
+    print("Exception when calling UsersApi->get_users: %s\n" % e)
+
+
 
 # CLIENT_ID = os.environ["GENESYSCLOUD_OAUTHCLIENT_ID"]
 # CLIENT_SECRET = os.environ["GENESYSCLOUD_OAUTHCLIENT_SECRET"]
